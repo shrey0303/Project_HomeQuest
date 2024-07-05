d8f179026495c3dbfcb9a973ddd4b34e3f03bc5b
Shrey Patel <95296438+shrey0303@users.noreply.github.com>
2024-07-06 00:36:03 +0530

    Update db.ts

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   mongoose
-    .connect("mongodb://localhost:27017/property")
+    .connect("mongodb+srv://jontywithbond007:BIQHdR1kCUglbECG@cluster0.numd2qo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("Connected to MongoDB."))
     .catch((error) => console.log(error));
 };

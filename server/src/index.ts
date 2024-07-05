4ee1b81b7cf07e7da27e6ae4843deb8a9a463797
Shrey Patel <95296438+shrey0303@users.noreply.github.com>
2024-07-05 19:32:15 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,7 @@ const bootstrapServer = async () => {
   //   })
   // );
   app.use(cors({
-    origin: 'https://home-quest-three.vercel.app',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true

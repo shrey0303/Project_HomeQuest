2c56eeba3c8d4659e846ba675d5645dbe5e33224
Shrey Patel <95296438+shrey0303@users.noreply.github.com>
2024-07-06 01:34:23 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import context from "./graphql/context";
 
 dotenv.config();
 const app = express();
-const port = Number(process.env.PORT) || 3000;
+const port = process.env.PORT || 3000;
 
 const bootstrapServer = async () => {
   await connectDb();

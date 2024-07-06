5f252f7235a169eba14c4c0e9236d601ab8b1a04
Shrey Patel <95296438+shrey0303@users.noreply.github.com>
2024-07-06 10:40:26 +0530

    Update index.tsx

--- client/src/components/layout/Providers/index.tsx
+++ client/src/components/layout/Providers/index.tsx
@@ -8,7 +8,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 const Providers = ({ children }: { children: ReactNode }) => {
   const client = new ApolloClient({
     uri:
-        import.meta.env.VITE_GRAPHQL_SERVER_URI ||
+        "https://project-home-quest-api.vercel.app/graphql" ||
       "http://localhost:5000/graphql",
     cache: new InMemoryCache(),
     credentials: 'include'
